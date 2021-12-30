ecd10406a68090863858d4ad6e9b1ea077b56f8f
AntonDubovitskii <gtoll@yandex.ru>
2021-12-30 16:09:38 +0400

    Домашнее задание для урока 2

---
+++ Dubovitskii_Anton_dz_2/task_2_1.py
@@ -0,0 +1,11 @@
+print((15 * 3), type(15 * 3))            # 45 <class 'int'>
+print(isinstance((15 * 3), int))         # True
+
+print((15 / 3), type(15 / 3))            # 5.0 <class 'float'>
+print(isinstance((15 / 3), float))       # True
+
+print((15 // 2),  type(15 // 2))         # 7 <class 'int'>
+print(isinstance((15 // 2), int))        # True
+
+print((15 ** 2),  type(15 ** 2))         # 225 <class 'int'>
+print(isinstance((15 ** 2), int))        # True
---
+++ Dubovitskii_Anton_dz_2/task_2_4.py
@@ -0,0 +1,24 @@
+def convert_name_extract(list_in: list) -> list:
+    """Извлекает имена из элементов и формирует список приветствий."""
+    list_out = []
+
+    x = 0
+    i = 0
+
+    while i < len(my_list):                  # каждый элемент списка разделяется по пробелу, берется последний
+        sup_list = my_list[i].split()        # по условию это всегда нужное имя
+        list_out.append(sup_list[-1])        # имена заносятся в отдельный список
+        i += 1
+
+    while x < len(list_out):
+        list_out[x] = f'Привет {list_out[x].title()}!'     # каждый элемент списка достраивается согласно требованиям
+        x += 1
+
+    return list_out
+
+
+my_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+
+result = convert_name_extract(my_list)
+print(result)
+
